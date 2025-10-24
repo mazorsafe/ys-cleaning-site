705d7d57191189b202676a5bfa6463435cc64a4b
mazorsafe <mazorsafe@gmail.com>
2025-10-24 23:20:39 +0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAKvLG6c65HnqOxQMJrBx9e7QqXDH3LuKQ",
+  authDomain: "ys-cleaning-crm.firebaseapp.com",
+  projectId: "ys-cleaning-crm",
+  storageBucket: "ys-cleaning-crm.appspot.com",
+  messagingSenderId: "1002450943932",
+  appId: "1:1002450943932:web:c6968c10355d739acd75b9",
+  measurementId: "G-D26LVJ778X"
+};
